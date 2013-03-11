f8b8288d1da4cef053bc9b496b3dff74655dd1fb
Tauraloke Esteru <tauraloke@gmail.com>
2013-03-11 08:39:41 +0400

    for github...

--- settings.php
+++ settings.php
@@ -4,3 +4,9 @@ define('HTTP_PORT', 12054);
 define('HTTP_ADDRESS', '0.0.0.0');
 
 define('VERSION', '0.5');
+
+define('PATH', dirname(__FILE__));
+define('DS', DIRECTORY_SEPARATOR);
+define('FOLDER4SAVING', 'save_file_here');
+
+define('VARFILE_PATH', PATH . DS . 'vars' . DS);
